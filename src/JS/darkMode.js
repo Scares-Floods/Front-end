6f9090e4efdee07dea8ff3435215a6550e9a9ad8
Kelwin <kelwin.sbastos21@gmail.com>
2025-06-04 22:38:56 -0300

    Colocando o dark mode (Apenas na home)

--- src/JS/darkMode.js
+++ src/JS/darkMode.js
@@ -1,41 +1,6 @@
-const carousel = document.getElementById('carousel');
-
-// Imagens de fundo
-const backgrounds = [
-  'url(../../src/assets/enchente1.jpg)',
-  'url(../../src/assets/enchente2.jpg)',
-  'url(../../src/assets/enchente3.jpg)'
-];
-
 // Botão de dark mode
 const toggleButton = document.getElementById('toggle-dark-mode');
 const icon = document.getElementById('icon-theme');
-
-let current = 0;
-
-// Função para mostrar slide atual
-function showSlide(index) {
-  carousel.style.backgroundImage = backgrounds[index];
-}
-
-// Proximo slide
-function nextSlide() {
-  current = (current + 1) % backgrounds.length;
-  showSlide(current);
-}
-
-// Slide anterior
-function prevSlide() {
-  current = (current - 1 + backgrounds.length) % backgrounds.length;
-  showSlide(current);
-}
-
-// Mudar automaticamente a cada 5 segundos
-setInterval(nextSlide, 5000);
-
-// Mostrar o primeiro slide ao carregar
-showSlide(current);
-
 
 toggleButton.addEventListener('click', () => {
   document.body.classList.toggle('dark-mode');
