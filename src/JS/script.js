baa90899f377de7c8a84b6abd7fc6cf50753fa9a
Kelwin <kelwin.sbastos21@gmail.com>
2025-06-06 17:33:22 -0300

    Adicionando o menu hamburguer

--- src/JS/script.js
+++ src/JS/script.js
@@ -13,6 +13,21 @@ const backgrounds = [
 
 let current = 0;
 
+function mudouTamanho(){
+  if(window.innerWidth >= 768){
+    itens.style.display = 'block';
+  } else {
+    itens.style.display = 'none';
+  }
+}
+
+function clickMenu(){
+  if (itens.style.display == 'block'){
+    itens.style.display = 'none'
+  }else{
+    itens.style.display = 'block'
+  }
+}
 // Função para mostrar slide atual
 function showSlide(index) {
   carousel.style.backgroundImage = backgrounds[index];
